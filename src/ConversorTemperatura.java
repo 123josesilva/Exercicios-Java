d644ab996edb7bb2a63790269298e656937ee1f5
ZeBunto <jose99.silva@catolicasc.edu.br>
2025-02-04 23:16:15 -0300

    Criando class ConversorTemperatura

--- src/ConversorTemperatura.java
+++ src/ConversorTemperatura.java
@@ -5,12 +5,26 @@ public class ConversorTemperatura {
     // ((x*9)/5)+32
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("por favor digite a temperatura em Celcius");
-        String userInput = scanner.next();
+        System.out.println("Olá");
+        System.out.println("Digite 1 para converter de Celsius para Fahrenheit");
+        System.out.print("Digite 2 para converter de Fahrenheit para Celsius:");
+        String inputMenu = scanner.next();
+        Number intOptionMenu = parseNumber(inputMenu);
+        if (intOptionMenu.intValue() == 1) {
+            System.out.print("Digite a temperatura em Celsius: ");
+            String userInput = scanner.next();
+            Number c = parseNumber(userInput);
+            System.out.print("Temperatura em Fahrenheit: ");
+            System.out.print(toFahrenheit(c));
+        } else if (intOptionMenu.intValue() == 2) {
+            System.out.print("Digite a temperatura em Fahrenheit: ");
+            String userInput = scanner.next();
+            Number f = parseNumber(userInput);
+            System.out.print("Temperatura em Celsius: ");
+            System.out.println(toCelsius(f));
+        }
+
         scanner.close();
-        Number c = parseNumber(userInput);
-        System.out.println("valor: ");
-        System.out.println(toFahrenheit(c));
     }
 
     public static Number parseNumber(String userInput) throws NumberFormatException {
@@ -24,5 +38,10 @@ public class ConversorTemperatura {
     public static Number toFahrenheit(Number c) {
         Number f = ((c.floatValue() * 9) / 5) + 32;
         return f;
+    }
+
+    public static Number toCelsius(Number c) {
+        Number celsius = ((c.floatValue() - 32) * 5) / 9;
+        return celsius;
     }
 }
